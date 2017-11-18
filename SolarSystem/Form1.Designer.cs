59f07855bcdefce03b0cee3cc75b2ac5181203df
stKhaDgar <33667544+stKhaDgar@users.noreply.github.com>
2017-11-18 15:09:41 +0200

    Added mercury, sun and logic

--- SolarSystem/Form1.Designer.cs
+++ SolarSystem/Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(30, 30);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(30, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(138, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(950, 557);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximumSize = new System.Drawing.Size(970, 600);
+            this.MinimumSize = new System.Drawing.Size(970, 600);
+            this.Name = "Form1";
+            this.Text = "SolarSystem";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- SolarSystem/Form1.cs
+++ SolarSystem/Form1.cs
@@ -12,9 +12,58 @@ namespace SolarSystem
 {
     public partial class Form1 : Form
     {
+        PictureBox sun = new PictureBox();
+        Mercury merc = new Mercury();
+
+       
         public Form1()
         {
             InitializeComponent();
+
+            sun = pictureBox2;
+            sun.Location = new Point(this.ClientRectangle.Width / 2 - 40, this.ClientRectangle.Height / 2 - 40);
+            merc.icon = pictureBox1;
+            merc.timer = new Timer();
+            merc.timer.Interval = 10;
+            merc.centerPositionX = this.ClientRectangle.Width / 2;
+            merc.centerPositionY = this.ClientRectangle.Height / 2;
+            merc.radiusOrbit = 58;
+            merc.speedSpin = 0.416;
+            merc.timer.Tick += new EventHandler(merc.timer_Tick);
+            merc.timer.Start();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Mercury
+    {
+        public int Xposition;
+        public int Yposition;
+        public int centerPositionX;
+        public int centerPositionY;
+        public int radiusOrbit;
+        public double speedSpin;
+        public int Size;
+        public PictureBox icon;
+        public Timer timer;
+        public double angle = 0;
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            Xposition = (int)((radiusOrbit + 20) * Math.Sin(angle) + (centerPositionX - 15));
+            Yposition = (int)((radiusOrbit + 20) * Math.Cos(angle) + (centerPositionY - 15));
+            icon.Location = new Point(Xposition, Yposition);
+            angle = angle + 0.01 * speedSpin;
         }
     }
 }
